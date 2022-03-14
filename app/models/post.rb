128c1f664d5647a29e1bea3e705bb6113561a472
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-03-14 18:01:09 -0400

    commit tree 86332d7431ca7260c125f9ef731bd008acb077f3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    # validates :title, presence: :true
+    # validates :content, length: {minimum: 250}
+    # validates :summary, length: {maximum: 250}
+    # validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :must_be_click_bait
+
+    def must_be_click_bait
+        click_bait=["Won't Believe",
+            "Secret",
+            "Top #{Integer}",
+            "Guess"]
+        unless (title.split & click_bait)
+            errors.add("Not catchy enough")
+        end
+    end
 end
